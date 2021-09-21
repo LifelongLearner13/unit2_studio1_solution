6523bbb7c05fb3a900fc4901a7d08a8c395b10a4
LifelongLearner13 <sierragregg@gmail.com>
2021-09-20 20:53:49 -0500

    finished

---
+++ studio1_solution/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace studio1_solution
+{
+    public static class Circle
+    {
+        public static double Diameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public static double Circumference(double radius) => Math.PI * 2 * radius;
+
+        public static double Area(double radius) => Math.PI * radius * radius;
+    }
+
+    /* ----- CLASS Version ----- */
+    //public class Circle
+    //{
+    //    private double _radius;
+    //    public Circle(double radius)
+    //    {
+    //        _radius = radius;
+    //    }
+
+    //    public double Diameter => 2 * _radius;
+
+    //    public double Circumference => Math.PI * 2 * _radius;
+
+    //    public double Area => Math.PI * _radius * _radius;
+    //}
+}
---
+++ studio1_solution/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace studio1_solution
+{
+    class Program
+    {
+        /* Reduce duplication of code */
+        static string UserPrompt()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Radius of the circle ('q' to quite)?");
+            return Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string userInput = UserPrompt();
+
+            /* Using a `while` over a `do - while` to allow user to quit before entering first values */
+            while (!userInput.ToLower().Equals("q"))
+            {
+                double num;
+                while(userInput.Equals("") || !Double.TryParse(userInput, out num))
+                {
+                    Console.WriteLine("Must be a valid number.");
+
+                    userInput = UserPrompt();
+                }
+
+                double r = Double.Parse(userInput);
+
+                while(r < 0.0)
+                {
+                    Console.WriteLine("Must be a positive number.");
+
+                    userInput = UserPrompt();
+                    r = Double.Parse(userInput);
+                }
+
+                Console.WriteLine("The area of the circle is: " + Circle.Area(r));
+                Console.WriteLine("The circumference of the circle is: " + Circle.Circumference(r));
+                Console.WriteLine("The diameter of the circle is: " + Circle.Diameter(r));
+
+                Console.WriteLine("How many miles per gallon does your car get?");
+                string mpgString = Console.ReadLine();
+                double mpg = Double.Parse(mpgString);
+
+                double gallonsUsed = Circle.Circumference(r) / mpg;
+                Console.WriteLine("You need " + gallonsUsed + " gallons to travel around this circle");
+
+                /* Setup for next loop */
+                userInput = UserPrompt();
+            }
+        }
+    }
+}
